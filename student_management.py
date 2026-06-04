dcb275dbc0172844ef5749b25ba3a4c7e5ded7f3
lakshmigeethikamudunuri-pixel <lakshmigeethikamudunuri@gmail.com>
2026-06-04 12:55:33 +0530

    Create student_management.py

---
+++ student_management.py
@@ -0,0 +1,37 @@
+students = {}
+
+while True:
+    print("\n1.Add Student")
+    print("2.View Students")
+    print("3.Search Student")
+    print("4.Delete Student")
+    print("5.Exit")
+
+    choice = int(input("Enter choice: "))
+
+    if choice == 1:
+        roll = input("Enter Roll No: ")
+        name = input("Enter Name: ")
+        students[roll] = name
+
+    elif choice == 2:
+        for roll, name in students.items():
+            print(roll, "-", name)
+
+    elif choice == 3:
+        roll = input("Enter Roll No: ")
+        if roll in students:
+            print("Name:", students[roll])
+        else:
+            print("Student not found")
+
+    elif choice == 4:
+        roll = input("Enter Roll No: ")
+        if roll in students:
+            del students[roll]
+            print("Deleted")
+        else:
+            print("Student not found")
+
+    elif choice == 5:
+        break
